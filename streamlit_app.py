d7623567865ade4f372a318c887de1bf1bd279a3
Alex-Fond <a.vandenreek@student.fontys.nl>
2024-10-10 13:32:41 +0200

    Please work

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-st.title("🎈 My new app")
+st.title("My app")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "For help and documentation, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
 uploaded_file = st.file_uploader("Upload planning (Excel)", type="xlsx")
 df_uploaded = pd.read_excel(uploaded_file)
 
-st.write(df_uploaded.head())
+st.write(uploaded_file.name)
